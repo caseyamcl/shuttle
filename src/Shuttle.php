42746f0cde7a65f1202151938d0f8dc570d2f4c6
Casey McLaughlin <caseyamcl@gmail.com>
2018-06-12 15:37:00 -0400

    Refactor of Shuttle

--- src/Shuttle.php
+++ src/Shuttle.php
@@ -2,6 +2,20 @@
 
 namespace Shuttle;
 
+use SebastianBergmann\FileIterator\Iterator;
+use Shuttle\Event\AbortEvent;
+use Shuttle\Event\ActionResultInterface;
+use Shuttle\Event\MigrateFailedEvent;
+use Shuttle\Event\MigrateProcessedEvent;
+use Shuttle\Event\MigrateSkippedEvent;
+use Shuttle\Event\PrePersistEvent;
+use Shuttle\Event\ReadSourceEvent;
+use Shuttle\Event\RevertFailedEvent;
+use Shuttle\Event\RevertProcessedEvent;
+use Shuttle\Event\RevertSkippedEvent;
+use Shuttle\Exception\AlreadyMigratedException;
+use Shuttle\Exception\UnmetDependencyException;
+use Shuttle\Migrator\MigratorInterface;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
@@ -54,34 +68,51 @@ class Shuttle
      *
      * @param string $migratorName
      * @param iterable|string[] $sourceIds Source IDs to migrate, or null for all
-     * @param callable|null $continue  A callable that returns TRUE for continue and FALSE to stop
+     * @param callable|null $continue A callable that accepts the last action, or null (return TRUE to continue)
      */
     public function migrate(string $migratorName, ?iterable $sourceIds = null, callable $continue = null)
     {
         $migrator = $this->getMigrators()->get($migratorName);
-        $continue = $continue ?: function() { return true; };
+        $continue = $continue ?: function () {
+            return true;
+        };
 
         // Build an iterator
+        /** @var Iterator $iterator */
         if ($sourceIds) {
-            $iterator = function() use ($migrator, $sourceIds) {
+            $iterator = function () use ($migrator, $sourceIds) {
                 foreach ($sourceIds as $sourceId) {
                     yield $migrator->getSourceItem($sourceId);
                 }
             };
         } else {
             $iterator = $migrator->getSourceIterator();
+            if (is_array($iterator)) {
+                $iterator = new \ArrayIterator($iterator);
+            }
         }
 
-        /** @var SourceItem $sourceItem */
-        foreach ($iterator as $sourceItem) {
+        // Loop
+        for ($iterator->rewind(), $lastAction = null; $iterator->valid(); $iterator->next()) {
 
-            if (! $continue()) {
-                return;
+            if (! $continue) {
+                $this->eventDispatcher->dispatch(
+                    ShuttleEvents::ABORT,
+                    new AbortEvent(ShuttleAction::MIGRATE, (string) $migrator, $lastAction)
+                );
+                break;
             }
 
-            $prepared = $migrator->prepare($sourceItem);
-            $destinationId = $migrator->persist($prepared);
-            $migrator->recordMigrate($sourceItem, $destinationId);
+            try {
+                /** @var SourceItem $sourceItem */
+                $sourceItem = $iterator->current();
+                $result = $this->migrateItem($migrator, $sourceItem);
+            } catch (\Throwable $e) {
+                $result = new MigrateFailedEvent($migratorName, 'unknown', 'Failed to read source item');
+                $this->getEventDispatcher()->dispatch(ShuttleEvents::MIGRATE_RESULT, $result);
+            }
+
+            $lastAction = $result;
         }
     }
 
@@ -90,27 +121,114 @@ class Shuttle
      *
      * @param string $migratorName
      * @param iterable|string[] $sourceIds Source IDs to migrate, or null for all
-     * @param callable|null $continue  A callable that returns TRUE for continue and FALSE to stop
+     * @param callable|null $continue A callable that accepts the last action, or null (return TRUE to continue)
      */
     public function revert(string $migratorName, ?iterable $sourceIds = null, callable $continue = null)
     {
         $migrator = $this->getMigrators()->get($migratorName);
-        $continue = $continue ?: function() { return true; };
+        $continue = $continue ?: function () {
+            return true;
+        };
 
         $iterator = $sourceIds ?: function () use ($migrator) {
-            foreach ($migrator->getReport() as $record) {
+            foreach ($migrator->getMigrateRecords() as $record) {
                 yield $record->getSourceId();
             }
         };
 
         /** @var string[] $sourceId */
+        $lastAction = null;
         foreach ($iterator as $sourceId) {
 
-            if (! $continue()) {
-                return;
+            if (!$continue()) {
+                $this->eventDispatcher->dispatch(
+                    ShuttleEvents::ABORT,
+                    new AbortEvent(ShuttleAction::REVERT, (string) $migrator, $lastAction)
+                );
+                break;
             }
 
-            $migrator->remove($sourceId);
+            $lastAction = $this->revertItem($migrator, $sourceId);
         }
+    }
+
+    /**
+     * Migrate an item
+     *
+     * @param MigratorInterface $migrator
+     * @param SourceItem $sourceItem
+     * @return ActionResultInterface
+     */
+    private function migrateItem(MigratorInterface $migrator, SourceItem $sourceItem): ActionResultInterface
+    {
+        try {
+            $this->eventDispatcher->dispatch(
+                ShuttleEvents::READ_SOURCE_RECORD,
+                new ReadSourceEvent($sourceItem, (string) $migrator)
+            );
+
+            $prepared = $migrator->prepare($sourceItem);
+
+            $this->eventDispatcher->dispatch(
+                ShuttleEvents::PRE_PERSIST,
+                new PrePersistEvent($sourceItem, $prepared, (string) $migrator)
+            );
+
+            $destinationId = $migrator->persist($prepared, $sourceItem);
+            $result = new MigrateProcessedEvent((string) $migrator, $sourceItem, $destinationId);
+
+        } catch (AlreadyMigratedException $e) {
+            $result = new MigrateSkippedEvent(
+                (string) $migrator,
+                $sourceItem->getId(),
+                'Item is already migrated'
+            );
+        }
+        catch (UnmetDependencyException $e) {
+            $result = new MigrateFailedEvent(
+                (string) $migrator,
+                $sourceItem->getId(),
+                'Unmet dependency: ' . $e->getMessage(),
+                $e
+            );
+        }
+        catch (\Throwable $e) {
+            $result = new MigrateFailedEvent(
+                (string) $migrator,
+                $sourceItem->getId(),
+                'An unexpected error occurred',
+                $e
+            );
+        }
+
+        $this->getEventDispatcher()->dispatch(ShuttleEvents::MIGRATE_RESULT, $result);
+        return $result;
+    }
+
+    /**
+     * Revert an item
+     *
+     * @param MigratorInterface $migrator
+     * @param string $sourceId
+     * @return ActionResultInterface
+     */
+    private function revertItem(MigratorInterface $migrator, string $sourceId): ActionResultInterface
+    {
+        try {
+            if ($migrator->isMigrated($sourceId)) {
+                $actuallyDeleted = $migrator->remove($sourceId);
+                $result = new RevertProcessedEvent((string) $migrator, $sourceId, $actuallyDeleted);
+
+            } else {
+                $result = new RevertSkippedEvent((string) $migrator, $sourceId, 'Destination record no longer found');
+            }
+
+        }
+        catch (\Throwable $e) {
+            $result = new RevertFailedEvent((string) $migrator, $sourceId, 'An unexpected error occurred', $e);
+        }
+
+        $this->getEventDispatcher()->dispatch(ShuttleEvents::REVERT_RESULT, $result);
+        return $result;
     }
 }
