1c524e4d4231bad4a687e31297ba43c13c866758
Casey McLaughlin <caseyamcl@gmail.com>
2018-06-18 20:21:45 -0400

    Whoops.. that wasn't the issue.

--- src/Recorder/Recorder.php
+++ src/Recorder/Recorder.php
@@ -172,6 +172,9 @@ class Recorder implements RecorderInterface
             ['unsigned' => true, 'notnull' => true, 'autoincrement' => true]
         );
         $table->setPrimaryKey(['rowid']);
+
+        $table->addIndex(['source_id']);
+        $table->addIndex(['destination_id']);
         $table->addUniqueIndex(['type', 'source_id']);
         $table->addUniqueIndex(['type', 'destination_id']);
 
